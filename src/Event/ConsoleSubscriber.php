8bf0e527917790e324c89ad5c271905d37c7df3e
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-05-13 20:18:42 -0400

    Update ConsoleSubscriber.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Event/ConsoleSubscriber.php
+++ src/Event/ConsoleSubscriber.php
@@ -4,11 +4,28 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Console\Event;
 
+use Ghostwriter\Container\Contract\ContainerInterface;
 use Ghostwriter\EventDispatcher\Contract\ListenerProviderInterface;
+use Ghostwriter\EventDispatcher\Contract\SubscriberInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 final class ConsoleSubscriber implements SubscriberInterface
 {
+    private ContainerInterface $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
     public function __invoke(ListenerProviderInterface $listenerProvider): void
     {
+        $listenerProvider->addListener(function (object $message) {
+            $this->container->get(OutputInterface::class)->write($message::class);
+        });
+
+        $listenerProvider->addListener(function (object $message) {
+            $this->container->get(OutputInterface::class)->writeln($message::class);
+        });
     }
 }
