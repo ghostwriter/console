2c013dbd4543cdb732d0b0939905dd2bec48aa1a
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-05-13 20:18:32 -0400

    Update CommandServiceProvider.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/ServiceProvider/CommandServiceProvider.php
+++ src/ServiceProvider/CommandServiceProvider.php
@@ -6,10 +6,18 @@ namespace Ghostwriter\Console\ServiceProvider;
 
 use Ghostwriter\Container\Contract\ContainerInterface;
 use Ghostwriter\Container\Contract\ServiceProviderInterface;
+use Symfony\Component\Console\Command\Command;
 
 final class CommandServiceProvider implements ServiceProviderInterface
 {
+    /** @var array<class-string<Command>> */
+    public array $commands = [];
+
     public function __invoke(ContainerInterface $container): void
     {
+        foreach ($this->commands as $command) {
+            $container->bind($command);
+            $container->tag($command, [Command::class]);
+        }
     }
 }
