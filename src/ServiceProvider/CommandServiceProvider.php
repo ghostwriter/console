5edea04c11d79191e2ee6dd6e689e5ecd62b8cfa
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-05-13 20:13:40 -0400

    Create CommandServiceProvider.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/ServiceProvider/CommandServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Console\ServiceProvider;
+
+use Ghostwriter\Container\Contract\ContainerInterface;
+use Ghostwriter\Container\Contract\ServiceProviderInterface;
+
+final class CommandServiceProvider implements ServiceProviderInterface
+{
+    public function __invoke(ContainerInterface $container): void
+    {
+    }
+}
