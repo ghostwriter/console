bb444a4389877055f7feaaafb6200b9cc33e41bd
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-11-20 00:01:45 -0600

    Add ghostwriter/container dependency and test ConsoleDefinition
    Added ghostwriter/container as a required dependency in composer.json. Updated ConsoleDefinitionTest to include an invocation test that verifies container aliasing for input, output, and style interfaces.
    
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Container/ConsoleDefinitionTest.php
+++ tests/Unit/Container/ConsoleDefinitionTest.php
@@ -5,7 +5,14 @@ declare(strict_types=1);
 namespace Tests\Unit\Container;
 
 use Ghostwriter\Console\Container\ConsoleDefinition;
+use Ghostwriter\Container\Interface\ContainerInterface;
 use PHPUnit\Framework\Attributes\CoversClass;
+use Symfony\Component\Console\Input\ArgvInput;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\ConsoleOutput;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\StyleInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 use Tests\Unit\AbstractTestCase;
 
 #[CoversClass(ConsoleDefinition::class)]
@@ -17,5 +24,20 @@ final class ConsoleDefinitionTest extends AbstractTestCase
             ConsoleDefinition::class,
             new ConsoleDefinition(),
         );
+    }
+
+    public function testInvoke(): void
+    {
+        $container = $this->createMock(ContainerInterface::class);
+
+        $container->expects(self::exactly(3))
+            ->method('alias')
+            ->willReturnMap([
+                [ArgvInput::class, InputInterface::class],
+                [ConsoleOutput::class, OutputInterface::class],
+                [SymfonyStyle::class, StyleInterface::class],
+            ]);
+
+        (new ConsoleDefinition())($container);
     }
 }
