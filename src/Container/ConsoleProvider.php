8af044daabe9530371f2ffbd01902a4662dc7912
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2026-09-09 22:30:57 -0500

    Update ConsoleProvider.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Container/ConsoleProvider.php
+++ src/Container/ConsoleProvider.php
@@ -4,13 +4,7 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Console\Container;
 
-use Ghostwriter\Console\Container\Symfony\Console\ApplicationFactory;
-use Ghostwriter\Console\Container\Symfony\Console\CommandLoader\ContainerCommandLoaderFactory;
-use Ghostwriter\Container\Interface\Service\FactoryInterface;
 use Ghostwriter\Container\Service\Provider\AbstractProvider;
-use Symfony\Component\Console\Application;
-use Symfony\Component\Console\CommandLoader\CommandLoaderInterface;
-use Symfony\Component\Console\CommandLoader\ContainerCommandLoader;
 use Symfony\Component\Console\Input\ArgvInput;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\ConsoleOutput;
