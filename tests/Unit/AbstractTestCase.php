f0176961927a629585fb3945a47976f42388969a
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2026-09-09 22:30:16 -0500

    Update AbstractTestCase.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/AbstractTestCase.php
+++ tests/Unit/AbstractTestCase.php
@@ -4,6 +4,97 @@ declare(strict_types=1);
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use Ghostwriter\Console\Container\Extension\AbstractApplicationExtension;
+use Ghostwriter\Console\Container\Factory\AbstractApplicationFactory;
+use Ghostwriter\Container\Interface\ContainerInterface;
+use Ghostwriter\PHPUnitAssertions\Trait\AssertionsTrait;
+use Mockery;
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+use Mockery\Generator\MockConfigurationBuilder;
+use Mockery\MockInterface;
+use Throwable;
 
-abstract class AbstractTestCase extends TestCase {}
+abstract class AbstractTestCase extends MockeryTestCase
+{
+    use AssertionsTrait;
+
+    /**
+     * @template TMock of object
+     *
+     * @param class-string<TMock> $class
+     *
+     * @throws Throwable
+     *
+     * @return MockInterface&TMock
+     */
+    final public function mock(string $class): MockInterface
+    {
+        $mockConfigurationBuilder = new MockConfigurationBuilder();
+
+        $mockConfigurationBuilder->addWhiteListedMethod('unset');
+
+        return Mockery::mock($class, $mockConfigurationBuilder);
+    }
+
+    /**
+     * @template TSpy of object
+     *
+     * @param class-string<TSpy> $class
+     *
+     * @throws Throwable
+     *
+     * @return MockInterface&TSpy
+     */
+    final public function spy(string $class): MockInterface
+    {
+        $mockConfigurationBuilder = new MockConfigurationBuilder();
+
+        $mockConfigurationBuilder->addWhiteListedMethod('unset');
+
+        return Mockery::spy($class, $mockConfigurationBuilder);
+    }
+
+    /**
+     * @template TStub of object
+     *
+     * @param class-string<TStub> $class
+     *
+     * @throws Throwable
+     *
+     * @return MockInterface&TStub
+     */
+    final public function stub(string $class): MockInterface
+    {
+        return $this->mock($class)->shouldIgnoreMissing();
+    }
+
+    /**
+     * @throws Throwable
+     *
+     * @return AbstractApplicationExtension&MockInterface
+     */
+    public function mockApplicationExtension(): AbstractApplicationExtension
+    {
+        return $this->mock(AbstractApplicationExtension::class);
+    }
+
+    /**
+     * @throws Throwable
+     *
+     * @return AbstractApplicationFactory&MockInterface
+     */
+    public function mockApplicationFactory(): AbstractApplicationFactory
+    {
+        return $this->mock(AbstractApplicationFactory::class);
+    }
+
+    /**
+     * @throws Throwable
+     *
+     * @return ContainerInterface&MockInterface
+     */
+    public function mockContainer(): ContainerInterface
+    {
+        return $this->mock(ContainerInterface::class);
+    }
+}
