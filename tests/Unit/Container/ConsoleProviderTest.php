fdf75b1f467e8b9a30a37339c92c186f12459d7d
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2026-09-09 22:30:27 -0500

    Update ConsoleProviderTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Container/ConsoleProviderTest.php
+++ tests/Unit/Container/ConsoleProviderTest.php
@@ -5,16 +5,10 @@ declare(strict_types=1);
 namespace Tests\Unit\Container;
 
 use Ghostwriter\Console\Container\ConsoleProvider;
-use Ghostwriter\Console\Container\Symfony\Console\ApplicationExtension;
-use Ghostwriter\Console\Container\Symfony\Console\ApplicationFactory;
-use Ghostwriter\Console\Container\Symfony\Console\CommandLoader\ContainerCommandLoaderFactory;
-use Ghostwriter\Container\Interface\ContainerInterface;
 use Ghostwriter\Container\Interface\Service\ProviderInterface;
 use Ghostwriter\Container\Service\Provider\AbstractProvider;
 use PHPUnit\Framework\Attributes\CoversClass;
-use Symfony\Component\Console\Application;
-use Symfony\Component\Console\CommandLoader\CommandLoaderInterface;
-use Symfony\Component\Console\CommandLoader\ContainerCommandLoader;
+use PHPUnit\Framework\Attributes\DataProvider;
 use Symfony\Component\Console\Input\ArgvInput;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\ConsoleOutput;
@@ -23,33 +17,32 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\StyleInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Tests\Unit\AbstractTestCase;
+use Throwable;
 
 #[CoversClass(ConsoleProvider::class)]
 final class ConsoleProviderTest extends AbstractTestCase
 {
-    public function testConsoleProviderRegister(): void
+    /** @throws Throwable */
+    #[DataProvider('provideConsoleProviderRegisterCases')]
+    public function testConsoleProviderRegister(object $consoleProvider): void
     {
-        $container = $this->createMock(ContainerInterface::class);
+        self::assertInstanceOf(ProviderInterface::class, $consoleProvider);
 
-        $container->expects(self::exactly(4))
-            ->method('alias')
-            ->withParameterSetsInAnyOrder(
-                [ConsoleOutputInterface::class, ConsoleOutput::class],
-                [InputInterface::class, ArgvInput::class],
-                [OutputInterface::class, ConsoleOutputInterface::class],
-                [StyleInterface::class, SymfonyStyle::class],
-            )->seal();
+        self::assertInstanceOf(AbstractProvider::class, $consoleProvider);
 
-        (new ConsoleProvider())->register($container);
+        $container = $this->mockContainer();
+
+        $container->expects('alias')->with(ConsoleOutputInterface::class, ConsoleOutput::class);
+        $container->expects('alias')->with(InputInterface::class, ArgvInput::class);
+        $container->expects('alias')->with(OutputInterface::class, ConsoleOutputInterface::class);
+        $container->expects('alias')->with(StyleInterface::class, SymfonyStyle::class);
+
+        $consoleProvider->register($container);
     }
 
-    public function testExtendsAbstractProvider(): void
+    /** @return iterable<string,array{0:ProviderInterface}> */
+    public static function provideConsoleProviderRegisterCases(): iterable
     {
-        self::assertInstanceOf(AbstractProvider::class, new ConsoleProvider());
-    }
-
-    public function testImplementsProviderInterface(): void
-    {
-        self::assertInstanceOf(ProviderInterface::class, new ConsoleProvider());
+        yield ConsoleProvider::class => [new ConsoleProvider()];
     }
 }
